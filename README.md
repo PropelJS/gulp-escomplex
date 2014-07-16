4af0dece5066de39835cbb0f7b53134bde0474a3
Jerry Sievert <code@legitimatesounding.com>
2014-07-16 10:00:41 -0700

    testing and docs

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This module does static analysis of source files and prepares them for a `reporter`.
 
 It uses `escomplex` to do the heavy lifting.
+
+```
+gulp.task('complexity', function () {
+  return gulp.src([
+    'index.js',
+    'gulpfile.js'
+  ])
+  .pipe(complexity());
+});
+```
---
+++ test/complexity.js
@@ -0,0 +1,32 @@
+var test = require('tape'),
+    vinyl = require('vinyl'),
+    complex = require('../');
+
+var textContentsInput = 'function foo (test) { console.log(test); }';
+
+var testFile = new vinyl({
+  cwd: "/home/jerry/work",
+  base: "/home/jerry/work/test",
+  path: "/home/jerry/work/test/file.js",
+  contents: new Buffer(textContentsInput)
+});
+
+test('should run complexity on files', function (t) {
+  t.plan(7);
+
+  var stream = complex();
+  stream.on('data', function (newFile) {
+    t.ok(newFile, 'emits a file');
+    t.ok(newFile.path, 'file has a path');
+    t.ok(newFile.contents, 'file has contents');
+    t.ok(newFile.analysis, 'file has complexity information');
+    t.ok(newFile instanceof vinyl, 'file is Vinyl');
+    t.ok(newFile.analysis instanceof Buffer, 'file analysis are a buffer');
+
+    var analysis = JSON.parse(newFile.analysis.toString('utf8'));
+
+    t.ok(analysis.aggregate, 'analysis should have an aggregate');
+  });
+
+  stream.write(testFile);
+});
